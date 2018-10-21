9a6f84f72b301822663d867d0188bb262d607f5b
WrongNub <44216624+WrongNub@users.noreply.github.com>
2018-10-20 22:41:43 -0400

    Update chloe.py

--- chloe.py
+++ chloe.py
@@ -42,6 +42,43 @@ async def cooldown_error(error, ctx):
 #CONFIRGURING
 
 
+#UTILITY
+@client.command(pass_context=True)
+async def nickname(ctx, member: discord.User=None, *, newnick=None):
+    author = ctx.message.author
+    try:
+        if ctx.message.author.server_permissions.manage_nicknames:
+            if member is None:
+                embed = discord.Embed(color=0xff0200)
+                embed.add_field(name=':x: Error:', value='```Please specify a user!```', inline=False)
+                embed.set_footer(icon_url=author.avatar_url, text="| Utility commands!")
+                await client.say(embed=embed)
+            
+        else:
+            embed = discord.Embed(color=0xff0200)
+            embed.add_field(name=':x: Error', value='You are missing the following permission: ``Manage Nicknames``', inline=False)
+            embed.set_footer(icon_url=author.avatar_url, text='You cant use this command!')
+            await client.say(embed=embed)
+            
+        await client.change_nickname(member, newnick)
+        embed = discord.Embed(color=0x00ff00)
+        embed.add_field(name='Changed:', value=f"You have changed {member.name}'s name to `{newnick}`", inline=True)
+        embed.set_footer(icon_url=author.avatar_url, text="| Utility commands!")
+        await client.say(embed=embed) 
+            
+    except discord.Forbidden:
+        embed = discord.Embed(color=0xff0200)
+        author = ctx.message.author
+        embed.set_author(name="Something went wrong ;-;")
+        embed.add_field(name=":x: Error", value="I'm missing the following permission: ```Manage Nicknames```", inline=False)
+        embed.set_footer(icon_url=author.avatar_url, text=f"Make sure my role is higher than {author.name}, that can be another error ;-;")
+        await client.say(embed=embed)
+
+    except discord.HTTPException:
+        embed = discord.Embed(color=0xff0200)
+        embed.add_field(name=":x: Error", value="Sorry, I can't nickname other bots at the moment ;-;")
+        await client.say(embed=embed)    
+
 #MODERARION COMMANDS:
   
     
