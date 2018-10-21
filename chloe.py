27cf7306d5d73475abef42597bfaea8ce028a90c
WrongNub <44216624+WrongNub@users.noreply.github.com>
2018-10-21 19:41:15 -0400

    Update chloe.py

--- chloe.py
+++ chloe.py
@@ -299,6 +299,35 @@ async def avatar(ctx, user: discord.Member = None):
         embed.set_image(url=user.avatar_url)
         await client.say(embed=embed)
 
+@client.command(pass_context=True)
+async def userinfo(ctx, user: discord.Member = None):
+    if user == None:
+        author = ctx.message.author
+        embed = discord.Embed(title="{}'s info".format(ctx.message.author.name), description="{}".format(ctx.message.author.mention), color=0x00ff00)
+        embed.add_field(name="Name:", value=ctx.message.author.name, inline=True)
+        embed.add_field(name="Nickname:", value=ctx.message.author.display_name, inline=True)
+        embed.add_field(name="ID:", value=ctx.message.author.id, inline=True)
+        embed.add_field(name="Status:", value=ctx.message.author.status, inline=True)
+        embed.add_field(name="Highest Role:", value=ctx.message.author.top_role)
+        embed.add_field(name="Account Created: ", value=ctx.message.author.created_at, inline=True)
+        embed.add_field(name="Joined at:", value=ctx.message.author.joined_at)
+        embed.set_thumbnail(url=ctx.message.author.avatar_url)
+        embed.set_footer(icon_url=author.avatar_url, text="Helpful commands!")
+        await client.say(embed=embed)
+        
+    else:
+        author = user
+        embed = discord.Embed(title="{}'s info".format(user.name), description="Here's the info.", color=0x00ff00)
+        embed.add_field(name="Name", value=user.name, inline=True)
+        embed.add_field(name="Nickname:", value=user.display_name, inline=True)
+        embed.add_field(name="ID", value=user.id, inline=True)
+        embed.add_field(name="Status", value=user.status, inline=True)
+        embed.add_field(name="Highest role", value=user.top_role)
+        embed.add_field(name="Account Created: ", value=user.created_at, inline=True)
+        embed.add_field(name="Joined at:", value=user.joined_at)
+        embed.set_thumbnail(url=user.avatar_url)
+        embed.set_footer(icon_url=author.avatar_url, text="Helpful commands!")
+        await client.say(embed=embed)      
 
 #MODERARION COMMANDS:
 
