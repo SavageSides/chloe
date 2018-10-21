dc3f0b984887ebd71fa302c4922cf87c9c497e54
SavageSides <42398425+SavageSides@users.noreply.github.com>
2018-10-21 09:23:09 -0500

    Update chloe.py

--- chloe.py
+++ chloe.py
@@ -230,7 +230,7 @@ async def purge(ctx, *, amount: int = None):
     with open("Mod-data.json", "r") as f:
         mod = json.load(f)
     modchannel = mod[ctx.message.server.id]["mod-channel"]
-    channels = discord.utils.get(ctx.message.server.channel, name = modchannel)
+    channels = discord.utils.get(ctx.message.server.channels, name = modchannel)
     try:
         if ctx.message.author.server_permissions.manage_messages:
             if amount is None:
